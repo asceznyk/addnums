fc28adb0dfe4152c13cc8c3acec4f435fa22f996
asceznyk <asceznyk@gmail.com>
2025-04-21 12:30:06 +0530

    added support for '\n'

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 def add(numbers:str) -> int:
   '''
   the function returns the sum of the numbers in input
-  numbers: is a string of comma seperated numbers
+  numbers: is a string of numbers seperated by comma and newline
   '''
   ans = 0
-  for x in numbers.split(','):
+  pnums = numbers.split('\n')
+  for x in pnums:
     if x == '': continue
+    if ',' in x:
+      ans += sum([int(y) for y in x.split(',')])
+      continue
     ans += int(x)
   return ans
 
--- test_main.py
+++ test_main.py
@@ -9,3 +9,6 @@ def test_add_single():
 def test_add_multiple():
   assert add("89,2,34") == 125
 
+def test_add_newline():
+  assert add("1\n2,3\n4") == 10
+
